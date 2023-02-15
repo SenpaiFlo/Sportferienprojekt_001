46f4e9d98b0e561091d2873b54a5c96ea77c91b4
SenpaiFlo <florin.curiger@gmx.ch>
2023-02-15 13:35:17 +0100

    Update Repository

---
+++ 05_software_installing/software_deploying.ps1
@@ -0,0 +1,23 @@
+# Installing Google Chrome
+choco install googlechrome
+
+# Installing Firefox
+choco install firefox
+
+# Installing Acrobat Reader
+choco install adobereader
+
+# Installing TeamViewer
+choco install teamviewer9
+
+# Installing FileZilla
+choco install filezilla
+
+# Installing Python 3.11
+choco install python
+
+# Installing 7Zip
+choco install 7zip
+
+# Installing Libre office
+choco install libreoffice-fresh
